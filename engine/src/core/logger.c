91521200b9c500d5fcfbef2a202c22866c4dfedf
Brandon Haley <brandon.haley94@gmail.com>
2025-03-17 21:15:29 -0400

    Changed the logger to use the platform specific console print.

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,10 +1,12 @@
 #include "logger.h"
 #include "asserts.h"
+#include "platform/platform.h"
 
-//TODO: remove this import, we'll be writing a much more sophisticated logging system soon
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
+
+#define OUTPUT_LENGTH 32000
 
 b8 initialize_logging(){
   // TODO: create log file
@@ -18,8 +20,9 @@ void shutdown_logging(){
 void log_output(log_level level, const char* message, ...){
   const char* level_strings[6] = {"[FATAL]: ", "[ERROR]: ", "[WARN]:  ", "[INFO]:  ", "[DEBUG]: ", "[TRACE]: "};
 
+  b8 is_error = level < LOG_LEVEL_WARN;
   //32k char limit should be good enough for any error
-  char fmt_message[32000];
+  char fmt_message[OUTPUT_LENGTH];
   memset(fmt_message, 0, sizeof(fmt_message));
 
   //There's apparently some error with clang for this so we can't just use va_list. It's fine.
@@ -29,12 +32,16 @@ void log_output(log_level level, const char* message, ...){
   vsnprintf(fmt_message, sizeof(fmt_message), message, arg_ptr);
   va_end(arg_ptr);
 
-  char out_message[32000];
-  sprintf(out_message, "%s%s\n", level_strings[level], fmt_message);
+  char out_message[OUTPUT_LENGTH];
+  sprintf(out_message, "%s%s", level_strings[level], fmt_message);
 
-  printf("%s", out_message);
+  if(is_error){
+    platform_console_write_error(out_message, level);
+  }else{
+    platform_console_write(out_message, level);
+  }
 }
 
 void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line){
-  log_output(LOG_LEVEL_FATAL, "Assertion Failure: %s, Message: %s, in file: %s, line: %d\n", expression, message, file, line);
+  log_output(LOG_LEVEL_FATAL, "Assertion Failure: %s, Message: %s, in file: %s, line: %d", expression, message, file, line);
 }
