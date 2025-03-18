e86e1059bbbf6e1d4238a93d8d03f84faa5f2fbe
Brandon Haley <brandon.haley94@gmail.com>
2025-03-17 21:15:51 -0400

    Testbed updated to test out the different level output.

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,9 +1,17 @@
 #include <core/asserts.h>
 #include <platform/platform.h>
+#include <core/logger.h>
 
 int main(void){
   platform_state *plat_state = malloc(sizeof(platform_state));
-  platform_startup(plat_state, "Velora", 100, 100, 1000, 500);
+  platform_startup(plat_state, "Velora", 100, 100, 1280, 720);
+  VFATAL("Testeroni");
+  VERROR("Testeroni");
+  VWARN("Testeroni");
+  VINFO("Testeroni");
+  VDEBUG("Testeroni");
+  VTRACE("Testeroni");
   while(platform_pump_messages(plat_state)){}
+  platform_shutdown(plat_state);
   return 0;
 }
