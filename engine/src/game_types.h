179ffa26587f143dd464e0f0ce199f2be2723af9
Brandon Haley <brandon.haley94@gmail.com>
2025-03-24 06:14:46 -0700

    Updated game_types with the memory update.

--- engine/src/game_types.h
+++ engine/src/game_types.h
@@ -6,11 +6,11 @@
 
 typedef struct _game{
   application_config app_config;
-
+ 
   b8 (*initialize)(struct _game* game_inst);
   b8 (*update)(struct _game* game_inst, f32 delta_time);
   b8 (*render)(struct _game* game_inst, f32 delta_time);
   void (*on_resize)(struct _game* game_inst, u32 width, u32 height);
 
-  memory_block* state;
+  void* state;
 } game;
