5e79e4014599083a7a0e8561e514e26be7afb175
Brandon Haley <brandon.haley94@gmail.com>
2025-03-25 10:26:06 -0700

    Updated documentation

--- engine/src/container/darray.h
+++ engine/src/container/darray.h
@@ -5,6 +5,14 @@
 #define STARTING_LENGTH 1
 #define REALLOCATE_MULTIPLIER 2 // how much to increase memory allocation, 2 is doubling the memory
 
+/*!
+ * @struct darray darray.h "container/darray.h"
+ * @brief Dynamically allocated array that expands when needed
+ * @property void* data A pointer to the actual data
+ * @property u64 length The number of elements in the data
+ * @property u64 stride The size of an individual element
+ * @property u64 cap The max number of elements for the memory allocated so far
+ */
 typedef struct _darray{
   void* data;
   u64 length, // number of elements in data
@@ -12,38 +20,43 @@ typedef struct _darray{
     cap;      // max number of elements for memory allocated in data
 } darray;
 
-/* Creates a new dynamic array on the heap
- * @param The size of each individual item
- * @return A newly allocated dynamic array on the heap
+/*!
+ * @brief Creates a new dynamic array on the heap
+ * @param stride  The size of each individual item
+ * @result A newly allocated dynamic array on the heap
  */
 VAPI darray* darray_new(u64 stride);
 
-/* Copies the data from the pointer given into the end of the dynamic array
- * @param Pointer to dynamic array data is being added to
- * @param Data pointer to the item being added
- * @return Pointer to the dynamic array
+/*!
+ * @brief Copies the data from the pointer given into the end of the dynamic array
+ * @param arr Pointer to dynamic array data is being added to
+ * @param data  Data pointer to the item being added
+ * @result Pointer to the dynamic array
  */
 VAPI darray* darray_push(darray* arr, void* data);
 
-/* Copies the data from end of the darray and decreases the length by one. Doesn't zero out the final item.
- * @param Pointer to dynamic array item is popped out of
- * @param Pointer to deposit the data
- * @return Pointer to darray, returns NULL if dynamic array is empty
+/*!
+ * @brief Copies the data from end of the darray and decreases the length by one. Doesn't zero out the final item.
+ * @param arr Pointer to dynamic array item is popped out of
+ * @param dest  Pointer to deposit the data
+ * @result Pointer to darray, returns NULL if dynamic array is empty
  */
 VAPI darray* darray_pop(darray* arr, void* dest);
 
-/* Places data from data pointer to the index
- * @param Pointer to dynamic array
- * @param Pointer to data to be inserted
- * @param Index to place data
- * @return Pointer to darray, returns NULL if trying to insert beyond the length of the array
+/*!
+ * @brief Places data from data pointer to the index
+ * @param arr Pointer to dynamic array
+ * @param data  Pointer to data to be inserted
+ * @param index Index to place data
+ * @result Pointer to darray, returns NULL if trying to insert beyond the length of the array
  */
 VAPI darray* darray_insert(darray* arr, void* data, u64 index);
 
-/* Removes data from index and closes the gap
- * @param Pointer to dynamic array
- * @param Index to remove
- * @param Pointer to copy data into
+/*!
+ * @brief Removes data from index and closes the gap
+ * @param arr Pointer to dynamic array
+ * @param index Index to remove
+ * @param dest  Pointer to copy data into
  * @return Pointer to darray, returns NULL if index is beyond the length of the dynamic array
  */
 VAPI darray* darray_remove(darray* arr, u64 index, void* dest);
