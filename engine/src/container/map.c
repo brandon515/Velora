794b776bb20b8f263319884d87e1c3b5bcad2360
Brandon Haley <brandon.haley94@gmail.com>
2025-03-25 20:38:41 -0400

    Added function to free memory of int_map.

--- engine/src/container/map.c
+++ engine/src/container/map.c
@@ -8,11 +8,7 @@ int_map* map_new(u64 item_size){
   return ret_map;
 }
 
-u64 map_get(int_map* map, u64 key){
-  return 0;
-}
-
-void map_set(int_map* map, u64 key, void* item){
+u64 map_get_index(int_map* map, u64 key){
   int key_index = -1;
   u64* map_keys = (u64*)map->keys->data;
   for(int i = 0; i < map->keys->length; i++){
@@ -21,6 +17,20 @@ void map_set(int_map* map, u64 key, void* item){
       break;
     }
   }
+  return key_index;
+}
+
+b8 map_get_item(int_map* map, u64 key, void* dest){
+  int key_index = map_get_index(map, key);
+  if(key_index == -1){
+    return FALSE;
+  }
+  vcopy_memory(dest, map->data->data+(map->data->stride*key_index), map->data->stride);
+  return TRUE;
+}
+
+void map_set_item(int_map* map, u64 key, void* item){
+  int key_index = map_get_index(map, key);
   if(key_index == -1){ // The key doesn't exist, just slam it into the end
     map->keys = darray_push(map->keys, &key);
     map->data = darray_push(map->data, item);
@@ -28,4 +38,10 @@ void map_set(int_map* map, u64 key, void* item){
     darray_delete(map->data, key_index);
     map->data = darray_insert(map->data, item, key_index);
   }
+}
+
+VAPI void map_free(int_map* map){
+  darray_free(map->data);
+  darray_free(map->keys);
+  vfree(map, sizeof(map), MEMORY_TAG_INT_MAP);
 }
--- engine/src/container/map.h
+++ engine/src/container/map.h
@@ -9,5 +9,6 @@ typedef struct _int_map{
 } int_map;
 
 VAPI int_map* map_new(u64 item_size);
-VAPI u64 map_get(int_map* map, u64 key);
-VAPI void map_set(int_map* map, u64 key, void* item);
+VAPI b8 map_get_item(int_map* map, u64 key, void* dest);
+VAPI void map_set_item(int_map* map, u64 key, void* item);
+VAPI void map_free(int_map* map);
