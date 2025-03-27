7a04bfc4f3833d991246918236d888b058729537
Brandon Haley <brandon.haley94@gmail.com>
2025-03-27 13:10:19 -0400

    Initialized and shutdown the event system.

--- engine/src/entry.h
+++ engine/src/entry.h
@@ -4,6 +4,7 @@
 #include "core/application.h"
 #include "core/logger.h"
 #include "core/vmemory.h"
+#include "core/event.h"
 #include "game_types.h"
 
 extern b8 create_game(game* out_game);
@@ -11,7 +12,7 @@ extern b8 create_game(game* out_game);
 int main(void){
   initialize_memory();
   initialize_logging();
-
+  initiate_event_system();
   game game_inst;
   if(create_game(&game_inst) == FALSE){
     VFATAL("Could not create instance of the game.");
@@ -32,6 +33,7 @@ int main(void){
     VFATAL("Application could not run main loop");
     return -4;
   }
+  shutdown_event_system();
   shutdown_logging();
   shutdown_memory();
   return 0;
