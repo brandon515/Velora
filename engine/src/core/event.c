cf6c6fbedb9900c2a158f505a931ebb873124dff
Brandon Haley <brandon.haley94@gmail.com>
2025-03-27 13:08:10 -0400

    Event system created and documented.

--- engine/src/core/event.c
+++ engine/src/core/event.c
@@ -1,18 +1,24 @@
 #include "event.h"
 #include "container/darray.h"
 #include "container/map.h"
+#include "platform/platform.h"
 
 // typedef b8 (*event_listener)(event* event);
 
+typedef struct _event_listener_data{
+  u64 id;
+  event_listener function;
+} event_listener_data;
 
 static darray* event_queue = NULL;
 static int_map* event_listeners = NULL;
+static u64 listener_id = 0;
 
 void initiate_event_system(){
   event_queue = darray_new(sizeof(event));
   event_listeners = map_new(sizeof(darray*));
   for(int i = ENGINE_INPUT_KEYBOARD; i < ENGINE_EVENT_ID_END; i++){
-    darray* temp = darray_new(sizeof(event_listener));
+    darray* temp = darray_new(sizeof(event_listener_data));
     map_set_item(event_listeners, i, &temp);
   }
 }
@@ -29,26 +35,56 @@ void shutdown_event_system(){
 u64 register_listener(u64 e_type, event_listener func){
   darray* listener_array;
   if(map_get_item(event_listeners, e_type, &listener_array) == FALSE){
-    listener_array = darray_new(sizeof(event_listener));
+    listener_array = darray_new(sizeof(event_listener_data));
     map_set_item(event_listeners, e_type, &listener_array);
   }
-  u64 ret_id = listener_array->length;
-  darray_push(listener_array, func);
+  u64 ret_id = listener_id;
+  listener_id++;
+  event_listener_data dat = {
+    .id = ret_id,
+    .function = func,
+  };
+  darray_push(listener_array, &dat);
   return ret_id;
 }
 
 b8 queue_event(event* new_event){
+  darray_push(event_queue, new_event);
+  return TRUE;
+}
+
+b8 pump_events(f64 time_limit){
+  f64 start_time = platform_get_absolute_time();
+  event* queue = (event*)event_queue->data;
+  for(int i = 0; i < event_queue->length; i++){
+    if(fire_event(&queue[i]) == FALSE){
+      VWARN("Event with id %d is being fired without any listeners", queue[i].event_type);
+    }
+    f64 benchmark_time = platform_get_absolute_time();
+    f64 duration = benchmark_time - start_time;
+    if(duration > time_limit){
+      VWARN("Event queue was not able to be cleared out in time");
+      darray_drain(event_queue, i);
+      return FALSE;
+    }
+  }
+  darray_clear(event_queue);
   return TRUE;
 }
 
 b8 fire_event(event* new_event){
-  darray* listener_array = darray_new(sizeof(event_listener));
-  if(map_get_item(event_listeners, new_event->event_type, listener_array) == FALSE){
-    map_set_item(event_listeners, new_event->event_type, listener_array);
+  darray* listener_array;
+  if(map_get_item(event_listeners, new_event->event_type, &listener_array) == FALSE){
+    listener_array = darray_new(sizeof(event_listener_data));
+    map_set_item(event_listeners, new_event->event_type, &listener_array);
+    return FALSE;
   }
-  event_listener* listeners = (event_listener*)listener_array->data;
+  if(listener_array->length == 0){
+    return FALSE;
+  }
+  event_listener_data* listeners = (event_listener_data*)listener_array->data;
   for(int i = 0; i < listener_array->length; i++){
-    if(listeners[i](new_event)){
+    if(listeners[i].function(new_event)){
       break;
     }
   }
@@ -56,5 +92,19 @@ b8 fire_event(event* new_event){
 }
 
 b8 deregister_listener(u64 e_type, u64 listener_id){
-  return TRUE;
+  darray* listener_array;
+  if(map_get_item(event_listeners, e_type, &listener_array) == FALSE){
+    return FALSE;
+  }
+  if(listener_array->length == 0){
+    return FALSE;
+  }
+  event_listener_data* listeners = (event_listener_data*)listener_array->data;
+  for(int i = 0; i < listener_array->length; i++){
+    if(listeners[i].id == listener_id){
+      darray_delete(listener_array, i);
+      return TRUE;
+    }
+  }
+  return FALSE;
 }
--- engine/src/core/event.h
+++ engine/src/core/event.h
@@ -22,14 +22,39 @@ typedef b8 (*event_listener)(event* event);
 void initiate_event_system();
 void shutdown_event_system();
 
-/* 
- * @function  register_listener
- * @abstract  Registers a listener to a specfic event
+/*!
+ * @brief Registers a listener to a specfic event
  * @param e_type  A u64 that signifies the type of event the listener wants to get
- * @param func  A function pointer that will process the event
- * @result The listenerID associated with that event. 
+ * @param func  A function pointer of event_listener type that will process the event
+ * @result The listenerID associated with that event. This is used to remove the listener later if needed
  */
 VAPI u64 register_listener(u64 e_type, event_listener func);
+
+/*!
+ * @brief Queues up an event to be dispatched in the main loop of the engine
+ * @param new_event  An event struct containing the data needed to process the event
+ * @result Returns TRUE if the event was able to be queued, FALSE if not
+ */
 VAPI b8 queue_event(event* new_event);
+
+/*!
+ * @brief Fires an event off to the listeners that have subscribed to it RIGHT NOW
+ * @param new_event  An event struct containing the data needed to process the event
+ * @result Returns FALSE if no listener is available for the event fired, retruns TRUE otherwise
+ */
 VAPI b8 fire_event(event* new_event);
+
+/*!
+ * @brief Detaches listener from a specific event
+ * @param e_type  A u64 that signifies the type of event the listener wants to be detatched from
+ * @param listener_id A u64 that was given when the listener asked to listen to the event
+ * @result Returns FALSE if the listener isn't attached to an event, TRUE if the listener was successfully detached
+ */
 VAPI b8 deregister_listener(u64 e_type, u64 listener_id);
+
+/*!
+ * @brief Works through the event queue within the time limit. The time limit may go over by a bit, the function won't stop processing an event only between events
+ * @param time_limit A float in seconds with the time limit for the event system to work through the queue
+ * @result Returns FALSE if the queue wasn't able to be emptied, TRUE if it was
+ */
+b8 pump_events(f64 time_limit);
