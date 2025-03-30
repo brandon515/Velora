49aac302274959c9efa3c4c9dc4b4167e965303e
Brandon Haley <brandon.haley94@gmail.com>
2025-03-30 13:42:54 -0400

    Updated build system with some defines for vulkan rendering.

--- engine/build.bat
+++ engine/build.bat
@@ -16,8 +16,8 @@ SET testFlags=-g -Wvarargs -Wall -Werror
 REM -Wall -Werror
 SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
 SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
-SET defines=-D_DEBUG -DVEXPORT -D_CRT_SECURE_NO_WARNINGS
-SET testDefines=-D_DEBUG -DVEXPORT -DTESTING -D_CRT_SECURE_NO_WARNINGS
+SET defines=-D_DEBUG -DVEXPORT -DVULKAN_RENDER -D_CRT_SECURE_NO_WARNINGS
+SET testDefines=-D_DEBUG -DVEXPORT -DVULKAN_RENDER -DTESTING -D_CRT_SECURE_NO_WARNINGS
 
 ECHO "Building %assembly%%..."
 clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
