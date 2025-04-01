865f462564715f4a9766e87f4335ee05772c146c
Brandon Haley <brandon.haley94@gmail.com>
2025-04-01 02:21:37 -0400

    Updated build batch file to include CPP compilation of VMA.

--- engine/build.bat
+++ engine/build.bat
@@ -2,23 +2,27 @@ REM Build script for engine
 @ECHO OFF
 SetLocal EnableDelayedExpansion
 
-REM Get a list of all the .c files
-SET cFilenames=
-FOR /R %%f in (*.c) do (
-  SET cFilenames=!cFilenames! %%f
-)
-
 REM echo "Files:" %cFilenames%
 
 SET assembly=engine
-SET compilerFlags=-g -shared -Wvarargs -Wall -Werror
+SET compilerFlags=-g -Wvarargs -Wall -Werror
 SET testFlags=-g -Wvarargs -Wall -Werror
 REM -Wall -Werror
 SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
-SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
+SET linkerFlags=-shared -luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
 SET defines=-D_DEBUG -DVEXPORT -DVULKAN_RENDER -D_CRT_SECURE_NO_WARNINGS
 SET testDefines=-D_DEBUG -DVEXPORT -DVULKAN_RENDER -DTESTING -D_CRT_SECURE_NO_WARNINGS
 
 ECHO "Building %assembly%%..."
-clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
-clang %cFilenames% %testFlags% -o ../bin/%assembly%_test.exe %testDefines% %includeFlags% %linkerFlags%
+REM Get a list of all the .c files
+SET objFiles=
+FOR /R %%f in (*.c) do (
+  clang %%f %compilerFlags% -c -o ../obj/%%~nxf.o %defines% %includeFlags%
+  SET objFiles=!objFiles! ../obj/%%~nxf.o
+)
+FOR /R %%f in (*.cpp) do (
+  clang++ -Wno-nullability-completeness -g -std=c++23 %%f -c -o ../obj/%%~nxf.o %includeFlags%
+  SET objFiles=!objFiles! ../obj/%%~nxf.o
+)
+clang %objFiles% -o ../bin/%assembly%.dll %linkerFlags%
+REM clang %cFilenames% %testFlags% -o ../bin/%assembly%_test.exe %testDefines% %includeFlags% %linkerFlags%
