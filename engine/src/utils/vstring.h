21b2c0da868ba4ab74ec7bd20110742021693b5b
Brandon Haley <brandon.haley94@gmail.com>
2025-04-06 16:54:04 -0400

    Clarified some documentation

--- engine/src/utils/vstring.h
+++ engine/src/utils/vstring.h
@@ -2,15 +2,17 @@
 
 #include "defines.h"
 
-/* Function takes the length of a 0 terminated string
- * @param Pointer to a zero terminated string
+/*! 
+ * @brief Function takes the length of a 0 terminated string
+ * @param str Pointer to a zero terminated string
  * @return Length of string in bytes minus the 0 termination
  */
 VAPI u64 vstrlen(const char* str);
 
-/* Compares two 0 terminated strings
- * @param First zero terminated string
- * @param Second zero terminated string
+/*!
+ * @brief Compares two 0 terminated strings
+ * @param str1 First zero terminated string
+ * @param str2 Second zero terminated string
  * @return TRUE if the strings are identical and the same length, FALSE otherwise
  */
 VAPI u8 vstrcmp(const char* str1, const char* str2);
