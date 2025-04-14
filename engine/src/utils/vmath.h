3e95e9290b667ca97c9b42632501ec7bbf23868c
Brandon Haley <brandon.haley94@gmail.com>
2025-04-13 23:20:57 -0400

    Added a vec2 union.

--- engine/src/utils/vmath.h
+++ engine/src/utils/vmath.h
@@ -11,4 +11,12 @@ typedef union _vec3{
   f32 xyz[3];
 } vec3;
 
+typedef union _vec2{
+  struct {
+    f32 x;
+    f32 y;
+  };
+  f32 xy[3];
+} vec2;
+
 vec3 multiply_scalar(vec3 vector, f32 scalar);
