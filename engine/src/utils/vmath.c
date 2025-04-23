fe28fd726617cb54add95f1030e370403f749c00
Brandon Haley <brandon.haley94@gmail.com>
2025-04-23 15:38:49 -0400

    Added some graphics matrix functions.

--- engine/src/utils/vmath.c
+++ engine/src/utils/vmath.c
@@ -8,6 +8,58 @@ vec3 multiply_scalar(vec3 vector, f32 scalar){
   return ret_vec;
 }
 
+mat4 identity_matrix(){
+  mat4 ret_matrix = {0};
+  for(int i = 0; i < 4; i++){
+    ret_matrix.mat[i*4+i] = 1;
+  }
+  return ret_matrix;
+}
+
+mat4 translation_matrix(vec3 translation){
+  mat4 ret_matrix = identity_matrix();
+  ret_matrix.a14 = translation.x;
+  ret_matrix.a24 = translation.y;
+  ret_matrix.a34 = translation.z;
+  return ret_matrix;
+}
+
+mat4 scaling_matrix(vec3 scale){
+  mat4 ret_matrix = identity_matrix();
+  for(int i = 0; i < 4; i++){
+    ret_matrix.mat[i*4+i] = scale.xyz[i];
+  }
+  return ret_matrix;
+}
+
+mat4 rotation_matrix(quat quaternion){
+  mat4 ret_matrix = identity_matrix();
+  f32 xx = quaternion.x*quaternion.x;
+  f32 xy = quaternion.x*quaternion.y;
+  f32 xz = quaternion.x*quaternion.z;
+  f32 xw = quaternion.x*quaternion.w;
+  
+  f32 yy = quaternion.y*quaternion.y;
+  f32 yz = quaternion.y*quaternion.z;
+  f32 yw = quaternion.y*quaternion.w;
+  
+  f32 zz = quaternion.z*quaternion.z;
+  f32 zw = quaternion.z*quaternion.w;
+
+  ret_matrix.mat[0]  = 1 - 2 * ( yy + zz );
+  ret_matrix.mat[1]  =     2 * ( xy - zw );
+  ret_matrix.mat[2]  =     2 * ( xz + yw );
+  ret_matrix.mat[4]  =     2 * ( xy + zw );
+  ret_matrix.mat[5]  = 1 - 2 * ( xx + zz );
+  ret_matrix.mat[6]  =     2 * ( yz - xw );
+  ret_matrix.mat[8]  =     2 * ( xz - yw );
+  ret_matrix.mat[9]  =     2 * ( yz + xw );
+  ret_matrix.mat[10] = 1 - 2 * ( xx + yy );
+
+  return ret_matrix;
+}
+
+
 u64 vclamp(u64 value, u64 minimum, u64 maximum){
   if(value < minimum){
     return minimum;
--- engine/src/utils/vmath.h
+++ engine/src/utils/vmath.h
@@ -19,7 +19,50 @@ typedef union _vec2{
   f32 xy[2];
 } vec2;
 
+typedef union _vec4 {
+  struct {
+    f32 x;
+    f32 y;
+    f32 z;
+    f32 w;
+  };
+  f32 xyzw[4];
+} vec4;
+
+typedef vec4 quat;
+
+/**
+ * @brief The member variables are ayx where y is the row and x is the column and the origin is the upper left
+ */
+typedef union _mat4{
+  struct {
+    f32 a11;
+    f32 a21;
+    f32 a31;
+    f32 a41;
+    f32 a12;
+    f32 a22;
+    f32 a32;
+    f32 a42;
+    f32 a13;
+    f32 a23;
+    f32 a33;
+    f32 a43;
+    f32 a14;
+    f32 a24;
+    f32 a34;
+    f32 a44;
+  };
+  f32 mat[16];
+} mat4;
+
 vec3 multiply_scalar(vec3 vector, f32 scalar);
+
+mat4 identity_matrix();
+
+mat4 translation_matrix(vec3 translation);
+mat4 scaling_matrix(vec3 scale);
+mat4 rotation_matrix(quat quaternion);
 
 /*! 
  * @brief Clamps the value between the minimum and maximum
