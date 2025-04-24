17eb6daae1383efa7450dc295c1cac6d3d5023dd
Brandon Haley <brandon.haley94@gmail.com>
2025-04-24 19:03:15 -0400

    Created a generic matrix multiplication function and a type safe wrapper function.

--- engine/src/utils/vmath.c
+++ engine/src/utils/vmath.c
@@ -8,16 +8,18 @@ vec3 multiply_scalar(vec3 vector, f32 scalar){
   return ret_vec;
 }
 
-mat4 identity_matrix(){
-  mat4 ret_matrix = {0};
-  for(int i = 0; i < 4; i++){
-    ret_matrix.mat[i*4+i] = 1;
+b8 identity_matrix(u64 rowsCols, f32* outMatrix){
+  for(int i = 0; i < rowsCols; i++){
+    for(int j = 0; j < rowsCols; j++){
+      outMatrix[i*rowsCols+j] = i==j?1:0;
+    }
   }
-  return ret_matrix;
+  return TRUE;
 }
 
 mat4 translation_matrix(vec3 translation){
-  mat4 ret_matrix = identity_matrix();
+  mat4 ret_matrix;
+  identity_matrix(4, ret_matrix.mat);
   ret_matrix.a14 = translation.x;
   ret_matrix.a24 = translation.y;
   ret_matrix.a34 = translation.z;
@@ -25,15 +27,16 @@ mat4 translation_matrix(vec3 translation){
 }
 
 mat4 scaling_matrix(vec3 scale){
-  mat4 ret_matrix = identity_matrix();
-  for(int i = 0; i < 4; i++){
+  mat4 ret_matrix = {0};
+  for(int i = 0; i < 3; i++){
     ret_matrix.mat[i*4+i] = scale.xyz[i];
   }
+  ret_matrix.mat[15] = 1;
   return ret_matrix;
 }
 
 mat4 rotation_matrix(quat quaternion){
-  mat4 ret_matrix = identity_matrix();
+  mat4 ret_matrix = {0};
   f32 xx = quaternion.x*quaternion.x;
   f32 xy = quaternion.x*quaternion.y;
   f32 xz = quaternion.x*quaternion.z;
@@ -55,10 +58,39 @@ mat4 rotation_matrix(quat quaternion){
   ret_matrix.mat[8]  =     2 * ( xz - yw );
   ret_matrix.mat[9]  =     2 * ( yz + xw );
   ret_matrix.mat[10] = 1 - 2 * ( xx + yy );
+  ret_matrix.mat[15] = 1;
 
   return ret_matrix;
 }
 
+b8 matrix_multiply(f32* mat1, u64 mat1Rows, u64 mat1Cols, f32* mat2, u64 mat2Rows, u64 mat2Cols, f32* outMatrix){
+  if(mat1Cols != mat2Rows){
+    return FALSE;
+  }
+  for(int mat1RowIndex = 0; mat1RowIndex < mat1Rows; mat1RowIndex++){
+    for(int mat2ColIndex = 0; mat2ColIndex< mat2Cols; mat2ColIndex++){
+      outMatrix[(mat1RowIndex*mat2Cols)+mat2ColIndex] = 0;
+      for(int i = 0; i < mat1Cols; i++){
+        /*u64 m1Index = (mat1RowIndex*mat1Cols)+i;
+        u64 m2Index = (i*mat2Cols)+mat2ColIndex;
+        u64 outIndex = (mat1RowIndex*mat2Cols)+mat2ColIndex;
+        f32 mat1Value = mat1[m1Index];
+        f32 mat2Value = mat2[m2Index];
+        f32 outMatrixvalue = mat1Value * mat2Value;*/
+        outMatrix[(mat1RowIndex*mat2Cols)+mat2ColIndex] += mat1[(mat1RowIndex*mat1Cols)+i] * mat2[(i*mat2Cols)+mat2ColIndex];
+      }
+    }
+  }
+  return TRUE;
+}
+
+void matrix_vec_multiply(f32* mat, f32* vec, u64 vecLength, f32* outVec){
+  matrix_multiply(mat, vecLength, vecLength, vec, vecLength, 1, outVec);
+}
+
+void matrix_vec4_multiply(mat4x4 mat, vec4 vec, vec4* outVec){
+  matrix_vec_multiply(mat.mat, vec.xyzw, 4, outVec->xyzw);
+}
 
 u64 vclamp(u64 value, u64 minimum, u64 maximum){
   if(value < minimum){
--- engine/src/utils/vmath.h
+++ engine/src/utils/vmath.h
@@ -56,13 +56,73 @@ typedef union _mat4{
   f32 mat[16];
 } mat4;
 
+typedef mat4 mat4x4;
+
+typedef union _mat2x4{
+  struct {
+    f32 a11;
+    f32 a21;
+    f32 a12;
+    f32 a22;
+    f32 a13;
+    f32 a23;
+    f32 a14;
+    f32 a24;
+  };
+  f32 mat[8];
+} mat2x4;
+
+typedef union _mat4x2{
+  struct {
+    f32 a11;
+    f32 a21;
+    f32 a31;
+    f32 a41;
+    f32 a12;
+    f32 a22;
+    f32 a32;
+    f32 a42;
+  };
+  f32 mat[8];
+} mat4x2;
+
 vec3 multiply_scalar(vec3 vector, f32 scalar);
 
-mat4 identity_matrix();
+b8 identity_matrix(u64 rowsCols, f32* outMatrix);
 
 mat4 translation_matrix(vec3 translation);
 mat4 scaling_matrix(vec3 scale);
 mat4 rotation_matrix(quat quaternion);
+
+/**
+ * @brief Multiply two arbitrary matrices together, the ordering is not arbitrary however
+ * @param mat1 A pointer to the f32 array for the first matrix
+ * @param mat1Rows How many rows in the first matrix
+ * @param mat1Cols How many columns in the first matrix
+ * @param mat2 A pointer to the f32 array for the second matrix
+ * @param mat2Rows How many rows in the seconds matrix
+ * @param mat2Cols How many columns in the second matrix
+ * @param outMatrix The zero'd out matrix to fill with the result. This function doesn't check it but it must be  mat1Rowsxmat2Cols size
+ * @return FALSE if mat1Cols and mat2Rows aren't equal, otherwise TRUE
+ */
+b8 matrix_multiply(f32* mat1, u64 mat1Rows, u64 mat1Cols, f32* mat2, u64 mat2Rows, u64 mat2Cols, f32* outMatrix);
+
+/**
+ * @brief Shorthand for multiplying a matrix with a vector.
+ * @param mat The matrix to multiply with the vector. This matrix needs to be vecLengthxvecLength in dimensions
+ * @param vec The vector to multiply with the matrix.
+ * @param vecLength The number of elements in the vector
+ * @param outVec The vector to put the results into
+ */
+void matrix_vec_multiply(f32* mat, f32* vec, u64 vecLength, f32* outVec);
+
+/**
+ * @brief Type casting for multiplying a 4x4 matrix with a 4 member vector
+ * @param mat A 4x4 matrix passed by reference
+ * @param vec A 4 member vector passed by reference
+ * @param outVec A pointer to the vector with the results
+ */
+void matrix_vec4_multiply(mat4x4 mat, vec4 vec, vec4* outVec);
 
 /*! 
  * @brief Clamps the value between the minimum and maximum
