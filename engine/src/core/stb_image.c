79edf905b7e549a74a7e7cee9fcd2d4667472e98
Brandon Haley <brandon.haley94@gmail.com>
2025-04-27 18:47:27 -0400

    Added STB image library. It's public domain and a single header.

---
+++ engine/src/core/stb_image.c
@@ -0,0 +1,14 @@
+#include "defines.h"
+#include "vmemory.h"
+
+void *stb_reallocate(void* oldPointer, u64 oldSize, u64 newSize){
+  vfree(oldPointer, oldSize, MEMORY_TAG_IMAGE);
+  return vallocate(newSize, MEMORY_TAG_IMAGE);
+}
+
+#define STBI_MALLOC(sz) vallocate(sz, MEMORY_TAG_IMAGE)
+#define STBI_REALLOC_SIZED(p, oldsz, newsz) stb_reallocate(p, oldsz, newsz)
+#define STBI_FREE(p) vfree(p, 0, MEMORY_TAG_IMAGE)
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
