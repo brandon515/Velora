d670b8126578ae771b5f09e2c87644751232757c
Brandon Haley <brandon.haley94@gmail.com>
2025-04-28 10:44:47 -0400

    Hooked stb_image macros into using engine functions.

--- engine/src/core/stb_image.c
+++ engine/src/core/stb_image.c
@@ -1,11 +1,13 @@
 #include "defines.h"
 #include "vmemory.h"
+#include "asserts.h"
 
 void *stb_reallocate(void* oldPointer, u64 oldSize, u64 newSize){
   vfree(oldPointer, oldSize, MEMORY_TAG_IMAGE);
   return vallocate(newSize, MEMORY_TAG_IMAGE);
 }
 
+#define STBI_ASSERT(x) VASSERT_MSG(x, "STB_Image ran into an error")
 #define STBI_MALLOC(sz) vallocate(sz, MEMORY_TAG_IMAGE)
 #define STBI_REALLOC_SIZED(p, oldsz, newsz) stb_reallocate(p, oldsz, newsz)
 #define STBI_FREE(p) vfree(p, 0, MEMORY_TAG_IMAGE)
