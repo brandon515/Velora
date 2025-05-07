305744f1542da221de55aa88f6767bf79775c1e4
Brandon Haley <brandon.haley94@gmail.com>
2025-05-07 11:51:50 -0400

    Added a find and reverse find functions.

--- engine/src/utils/vstring.c
+++ engine/src/utils/vstring.c
@@ -1,4 +1,5 @@
 #include "vstring.h"
+#include "core/vmemory.h"
 
 u64 vstrlen(const char* str){
   u64 len = 0;
@@ -26,4 +27,35 @@ u8 vstrcmp(const char* str1, const char* str2){
     }
   }
   return TRUE;
+}
+
+char* vsubstr(const char* str, u64 startIndex, u64 length){
+  u64 strLength = vstrlen(str);
+  if(startIndex >= strLength || startIndex+length >= strLength){
+    return NULL;
+  }
+  char* retStr = vallocate(length+1, MEMORY_TAG_STRING);
+  vcopy_memory(retStr, str+startIndex, length);
+  retStr[length] = 0;
+  return retStr;
+}
+
+u64 vrfind(const char* str, char character){
+  u64 strLength = vstrlen(str);
+  for(int i = strLength; i >= 0; i--){
+    if(str[i] == character){
+      return i;
+    }
+  }
+  return U64_MAX;
+}
+
+u64 vfind(const char* str, char character){
+  u64 strLength = vstrlen(str);
+  for(int i = 0; i <= strLength; i++){
+    if(str[i] == character){
+      return i;
+    }
+  }
+  return U64_MAX;
 }
--- engine/src/utils/vstring.h
+++ engine/src/utils/vstring.h
@@ -16,3 +16,28 @@ VAPI u64 vstrlen(const char* str);
  * @return TRUE if the strings are identical and the same length, FALSE otherwise
  */
 VAPI u8 vstrcmp(const char* str1, const char* str2);
+
+/**
+ * @brief Creates a substring that is [startIndex, startIndex+length] of str array
+ * @param str A pointer to a zero-terminated char array
+ * @param startIndex The position of the first letter to include in the substring
+ * @param length The length of the desired substring, not including the zero at the end of the string
+ * @return A pointer to a heap allocated zero-terminated char array or NULL if startIndex or startIndex+length is beyond the length of the string
+ */
+VAPI char* vsubstr(const char* str, u64 startIndex, u64 length);
+
+/**
+ * @brief Finds the last instance of the character provided
+ * @param str A pointer to a zero-terminated char array
+ * @param character The character to search for
+ * @return a u64 with the index of the character in the string or U64_MAX if none was found
+ */
+VAPI u64 vrfind(const char* str, char character);
+
+/**
+ * @brief Finds the first instance of the character provided
+ * @param str A pointer to a zero-terminated char array
+ * @param character The character to search for
+ * @return a u64 with the index of the character in the string or U64_MAX if none was found
+ */
+VAPI u64 vfind(const char* str, char character);
