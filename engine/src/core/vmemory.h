b8dcdd6e60ee203dbef189fb58e106a673d703b0
Brandon Haley <brandon.haley94@gmail.com>
2025-05-08 11:47:11 -0400

    Added new file memory tag.

--- engine/src/core/vmemory.h
+++ engine/src/core/vmemory.h
@@ -25,6 +25,7 @@
   MEM_TAG(MEMORY_TAG_INPUT_DATA)          \
   MEM_TAG(MEMORY_TAG_IMAGE)               \
   MEM_TAG(MEMORY_TAG_JSON)                \
+  MEM_TAG(MEMORY_TAG_FILE)                \
   MEM_TAG(MEMORY_TAG_END_TAG)             \
 
 #define GENERATE_ENUM(ENUM) ENUM,
