157252cd8d38956832863db242a2b71786bd4ecc
Brandon Haley <brandon.haley94@gmail.com>
2025-05-16 10:52:37 -0400

    Removed debug statement.

--- engine/src/core/vmemory.c
+++ engine/src/core/vmemory.c
@@ -38,10 +38,6 @@ void* vallocate(u64 size, memory_tag tag){
 
 void vfree(void* block, u64 size, memory_tag tag){
   platform_free(block, FALSE);
-
-  if(mem_stats.tagged_allocations[tag] < size){
-    VINFO("HERE");
-  }
   mem_stats.total_allocated -= size;
   mem_stats.tagged_allocations[tag] -= size;
 }
