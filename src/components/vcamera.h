74cc3df0ac68ccf9536bb5e872d0468528af67ce
Brandon Haley <brandon.haley94@gmail.com>
2026-04-15 20:33:30 -0400

    Created a helper function to get the pointer to the active camera and added a reference to the transform in the camera.

--- src/components/vcamera.h
+++ src/components/vcamera.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "defines.h"
+#include "components/vtransform.h"
 
 typedef struct _vcamera{
   b8 active;
+  vtransform *transform;
 }vcamera;
 
 b8 register_camera(u64 entityID, b8 active);
+
+vcamera* camera_get_active();
