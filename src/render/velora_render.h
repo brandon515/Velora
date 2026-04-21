4398a715ef32c21f761a365c34540ec2e453e4cd
Brandon Haley <brandon.haley94@gmail.com>
2026-04-20 21:01:09 -0400

    Created function prototypes to register a mesh and a texture.

--- src/render/velora_render.h
+++ src/render/velora_render.h
@@ -3,6 +3,10 @@
 #include "defines.h"
 #include "core/event.h"
 #include "platform/platform.h"
+#include "vmesh.h"
+#include "vmaterial.h"
+
+typedef u64 mesh_handle;
 
 typedef struct _render_state{
   void* internal_render_state;
@@ -17,3 +21,6 @@ b8 render_postframe(render_state* state);
 b8 resize_handler(event* newEvent, void* state);
 
 b8 initiate_render_system(render_state* state, const char* application_name, platform_state *plat_internal_state);
+
+b8 register_mesh(render_state* state, vmesh mesh, u64 *outHandle);
+b8 register_texture(render_state* state, velora_pixels mesh, u64 *outHandle);
