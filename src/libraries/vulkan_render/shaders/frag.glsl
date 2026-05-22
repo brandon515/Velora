c444a63eebf53db705cce29638b07c5623a64c3b
Brandon Haley <brandon.haley94@gmail.com>
2026-05-22 12:21:09 -0400

    Optimized shaders and added weights and joints.

--- src/libraries/vulkan_render/shaders/frag.glsl
+++ src/libraries/vulkan_render/shaders/frag.glsl
@@ -1,27 +1,16 @@
 #version 450
 #pragma shader_stage(fragment)
 
-const uint VELORA_MAX_OBJECTS = 256;
 const uint VELORA_MAX_MATERIALS = 256;
 const uint VELORA_TEXURES_PER_MATERIAL = 1;
 const uint VELORA_MAX_TEXTURES = VELORA_MAX_MATERIALS * VELORA_TEXURES_PER_MATERIAL;
 
-
-layout(binding = 0) uniform UniformBufferObject {
-    mat4 mvpMat;
-    uint textureIndex;
-} ubo[VELORA_MAX_OBJECTS];
-
 layout(binding = 1) uniform sampler2D texSampler[VELORA_MAX_TEXTURES];
 
 layout(location = 0) in vec2 texCoordOut;
+layout(location = 1) out uint texIndex;
 
 layout(location = 0) out vec4 outColor;
-
-layout( push_constant ) uniform constants
-{
-  uint uboIndex;
-};
 
 void main() {
     outColor = texture(texSampler[ubo[uboIndex].textureIndex], texCoordOut);
--- src/libraries/vulkan_render/shaders/vert.glsl
+++ src/libraries/vulkan_render/shaders/vert.glsl
@@ -6,7 +6,12 @@ const uint VELORA_MAX_OBJECTS = 256;
 layout(binding = 0) uniform UniformBufferObject {
     mat4 mvpMat;
     uint textureIndex;
+    uint skinOffset;
 } ubo[VELORA_MAX_OBJECTS];
+
+layout(binding = 2) uniform SkinMatrix{
+  mat4 skinMat
+} skinMatrix[VELORA_MAX_JOINTS];
 
 layout( push_constant, std430) uniform constants
 {
@@ -16,10 +21,14 @@ layout( push_constant, std430) uniform constants
 layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inNormal;
 layout(location = 2) in vec2 texCoord;
+layout(location = 3) in vec4 weights;
+layout(location = 4) in vec4 joints;
 
 layout(location = 0) out vec2 texCoordOut;
+layout(location = 1) out uint texIndex;
 
 void main() {
     gl_Position = ubo[uboIndex].mvpMat * vec4(inPosition, 1.0);
     texCoordOut = texCoord;
+    texIndex = ubo[uboIndex].textureIndex;
 }
